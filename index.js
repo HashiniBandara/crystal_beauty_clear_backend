7f069f7a829d7768d4ad1bbafc37ae4219ffe5c8
HashiniBandara <hashinicbandara@gmail.com>
2025-07-12 03:01:55 +0530

    update patch in index page 🖊

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import userRouter from './routes/userRouter.js';
-import productRouter from './routes/productRouter.js';
-import verifyJWT from './middleware/auth.js';
-import orderRouter from './routes/orderRouter.js';
-import categoryRouter from './routes/categoryRouter.js';
-import cors from 'cors';
-import dotenv from 'dotenv'; 
-import reviewRouter from './routes/reviewRouter.js';
-import contactRouter from './routes/contactRouter.js';
-import adminRouter from './routes/adminRouter.js';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import userRouter from "./routes/userRouter.js";
+import productRouter from "./routes/productRouter.js";
+import verifyJWT from "./middleware/auth.js";
+import orderRouter from "./routes/orderRouter.js";
+import categoryRouter from "./routes/categoryRouter.js";
+import cors from "cors";
+import dotenv from "dotenv";
+import reviewRouter from "./routes/reviewRouter.js";
+import contactRouter from "./routes/contactRouter.js";
+import adminRouter from "./routes/adminRouter.js";
 
 dotenv.config();
 
@@ -31,40 +31,31 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
   })
 );
 
-
-mongoose.connect(process.env.MONGO_URL).then(
-    ()=>{
-        console.log("Connected to the database");
-    }
-).catch(
-    ()=>{
-        console.log("Connection failed");
-    }
-)
-
-
-
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
 
 app.use(bodyParser.json());
-app.use(verifyJWT)
+app.use(verifyJWT);
 
-
-app.use("/api/user",userRouter)
-app.use("/api/product",productRouter)
-app.use("/api/order",orderRouter)
-app.use("/api/categories", categoryRouter)
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/categories", categoryRouter);
 app.use("/api/review", reviewRouter);
 app.use("/api/contact", contactRouter);
 app.use("/api/stats", adminRouter);
 
-
-app.listen(3000, 
-    ()=>{
-        console.log("Server is running on port 3000");
-    }
-)
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
